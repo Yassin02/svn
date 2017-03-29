b7d731de2805e9e825d504e7f32a7361044031fa
mp724299 <mp724299@MI103-13>
2017-03-29 17:09:42 +0200

    partage de projet

---
+++ tp/src/main/java/MyFirstTest.java
@@ -0,0 +1,13 @@
+
+public class MyFirstTest {
+
+	public void estEgale(int a, int b){
+		assert a != b : "a et b sont egaux";
+		
+	}
+	
+	public void estInferieure(int a, int b){
+		assert a < b : "a plus grand que b";
+	}
+	
+}
